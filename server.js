07ca8c8a9682f52a3598930edde07a9430f5a66e
Waleed <hafizwaleed0890@gmail.com>
2020-02-18 00:41:21 +0500

    Socket.io

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
 // const upload = require('./handlers/multer')
- 
-
 // require('./handlers/cloudinary');
-
+  
+io.on("connection" , socket => {
+    console.log("User Connected :D");
+    socket.on("chat message" , msg =>{
+        console.log(msg);
+        io.emit("chat message" , msg);
+    })
+} )
 //MiddleWare
 app.use(bodyParser.json());
 app.use(morgan())
@@ -15,7 +23,7 @@ app.use(morgan())
 require("./mongo");
 
 //Models
-require("./model/Signup");  
+require("./model/Signup");
 
 //Routes
 app.use("/signup", require("./routes/signup"));
@@ -24,23 +32,22 @@ app.use("/login", require("./routes/login"));
 
 
 
-
 //Middleware for 404 error generator
-app.use((req, res, next)=>{
-    req.status=404;
+app.use((req, res, next) => {
+    req.status = 404;
     const error = new Error("route not found");
-    next(error) ;
-    })
-    
-    //Error Handler
-app.use((error, req, res, next)=>{    
-    res.status(req.status || 500 ).send({
+    next(error);
+})
+
+//Error Handler
+app.use((error, req, res, next) => {
+    res.status(req.status || 500).send({
         message: error.message,
         stack: error.stack
-    });
-    });
-    
+    });  
+});
+
 const port = process.env.PORT || 5000;
-app.listen(port, ()=> {  
-console.log(`Umeed Server is running at port ${port}`);
-})
+server.listen(port, () => {
+    console.log(`Umeed Server is running at port ${port}`);
+})   
